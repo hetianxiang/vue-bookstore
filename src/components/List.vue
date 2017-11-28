2d7ddac1aad7d635b84546a3ece0df2c313ac4de
hetianxiang <rogermhe@gmail.com>
2017-11-28 17:39:53 +0800

    pro

---
+++ src/components/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+   <div class="col-md-3" >
+     <div class="panel panel-warning" v-for="book in books">
+       <div class="panel-heading" >
+         {{book.bookName}}
+       </div>
+       <div class="panel-body text-center" >
+         <img :src="book.bookCover" alt="" />
+       </div>
+       <div class="panel-footer">
+         {{book.bookPrice | currency('￥')}}
+         <router-link :to = "{name: 'detail', params: {id:book.id}}">查看详情</router-link>
+       </div>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    filters:{
+      currency(input,param1){
+        return param1 + input
+      }
+    },
+    created(){
+      axios.get('/book').then((res)=>{
+        this.books = res.data
+        console.log(res.data)
+      })
+    },
+    data(){
+      return {
+        books:[
+         
+        ]
+      }
+    },
+    component:{
+
+    },
+    methods:{}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img{
+  width:150px;
+  height: 200px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <div class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">bookstore</a>
+        </div>
+        <ul class="navbar-nav nav">
+          <li><router-link to = "/list">图书列表</router-link></li>
+          <li><router-link to = "/add">添加图书</router-link></li>   
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
